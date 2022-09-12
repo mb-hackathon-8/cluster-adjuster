dfc16bf5140256af168ef538f46f28b9be7727db
Ubuntu <nabil@happykhan.com>
2022-09-12 14:59:04 +0000

    profiles

--- scripts/acc_to_allele.py
+++ scripts/acc_to_allele.py
@@ -1,6 +1,33 @@
+"""
+Convert ssr to enterobase alleles 
+
+Convert ssr to enterobase alleles 
+
+### CHANGE LOG ###
+2022-09-12 Nabil-Fareed Alikhan <nabil@happykhan.com> 
+    * Initial build
+"""
+
+__licence__ = "ECL-2.0"
+__author__ = "Nabil-Fareed Alikhan"
+__author_email__ = "nabil@happykhan.com"
+__version__ = "0.1.0"
+
+
+epi = "Licence: " + __licence__ + " by " + __author__ + " <" + __author_email__ + ">"
+
+import argparse
 import time 
 import os 
+import traceback
+import sys 
 
+def main(args):
+    print(args)
+
+    with open(args.srr_file):
+        srr_list = [x.strip() for x in srr_file.readline()]
+        
 
 
 if __name__ == "__main__":
@@ -15,15 +42,15 @@ if __name__ == "__main__":
             "--version", action="version", version="%(prog)s " + __version__
         )
         parser.add_argument(
-            "-o", "--output", action="store", help="output prefix", default="summary.tsv"
+            "-o", "--output", action="store", help="output prefix", default="my_alleles"
         )
         parser.add_argument(
-            "app_dir", action="store", help="Path to directory of applications docx"
+            "srr_file", action="store", help="Path to SRR list"
         )
         args = parser.parse_args()
         if args.verbose:
             print("Executing @ " + time.asctime())
-        main()
+        main(args)
         if args.verbose:
             print("Ended @ " + time.asctime())
         sys.exit(0)
