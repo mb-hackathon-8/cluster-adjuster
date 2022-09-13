224a5d3ca312627278ab9e97b9f9287aafea2019
rfm-targa <rmamede@medicina.ulisboa.pt>
2022-09-13 11:36:30 +0100

    Updated script to extract clusters.

--- cluster_adjuster/extract_hiercc_clusters.py
+++ cluster_adjuster/extract_hiercc_clusters.py
@@ -12,10 +12,7 @@ import argparse
 import pandas as pd
 
 
-input_file = '/home/rmamede/Desktop/Hackathon/spyogenes/HierCC_wgMLST/wgMLST_hiercc.HierCC'
-output_directory = '/home/rmamede/Desktop/Hackathon/spyogenes/HierCC_wgMLST/hiercc_clusters'
-metadata_table = '/home/rmamede/Desktop/Hackathon/spyogenes/coelho_selected.tsv'
-def main(input_file, output_directory, metadata_table):
+def main(input_file, output_directory, metadata_table, columns):
 
     # create output_directory
     if os.path.isdir(output_directory) is False:
@@ -28,21 +25,31 @@ def main(input_file, output_directory, metadata_table):
     if metadata_table is not None:
         metadata = pd.read_csv(metadata_table, delimiter='\t', index_col=0, dtype=str)
 
+    if metadata_table is not None:
+        headers = '\t'.join(['Cluster', 'Samples'] + columns)
+    else:
+        headers = '\t'.join(['Cluster', 'Samples'])
     for c in hc_cuts:
         current_hc = hiercc_matrix[c]
         hc_clusters = list(current_hc.groupby(current_hc))
         hc_clusters = {c[0]: list(c[1].index) for c in hc_clusters}
 
         # get metadata counts/percentage
-        clusters_metadata = {}
-        for cid, csamples in hc_clusters.items():
-            metadata_counts = metadata['outbreak_number'][csamples].value_counts()
-            metadata_line = ','.join(['{0}({1})'.format(i, metadata_counts[i]) for i in metadata_counts.index])
-            clusters_metadata[cid] = metadata_line
+        if metadata_table is not None:
+            clusters_metadata = {}
+            for cid, csamples in hc_clusters.items():
+                for category in columns:
+                    metadata_counts = metadata[category][csamples].value_counts()
+                    metadata_line = ','.join(['{0}({1})'.format(i, metadata_counts[i]) for i in metadata_counts.index])
+                    clusters_metadata.setdefault(cid, []).append(metadata_line)
 
         # save file with clusters
         hc_output_file = os.path.join(output_directory, '{0}_clusters.tsv'.format(c))
-        hc_lines = ['{0}\t{1}\t{2}'.format(k, ','.join(v), clusters_metadata[k]) for k, v in hc_clusters.items()]
+        hc_lines = [headers]
+        if metadata_table is not None:
+            hc_lines += ['{0}\t{1}\t{2}'.format(k, ','.join(v), '\t'.join(clusters_metadata[k])) for k, v in hc_clusters.items()]
+        else:
+            hc_lines += ['{0}\t{1}'.format(k, ','.join(list(map(str, v)))) for k, v in hc_clusters.items()]
         with open(hc_output_file, 'w') as outfile:
             outfile.write('\n'.join(hc_lines)+'\n')
 
@@ -62,6 +69,10 @@ def parse_arguments():
 
     parser.add_argument('-m', '--metadata_table', type=str,
                         required=False, dest='metadata_table',
+                        help='')
+
+    parser.add_argument('-c', '--columns', type=str, nargs='+',
+                        required=False, dest='columns',
                         help='')
 
     args = parser.parse_args()
